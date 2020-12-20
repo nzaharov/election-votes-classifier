bd348a8d647f895eb96cd3920a47c120047ca3b9
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-12-20 21:20:15 +0200

    some implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::collections::hash_map::Entry::{Occupied, Vacant};
 #[warn(clippy::all)]
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
 use std::io;
 
@@ -33,43 +35,95 @@ impl VoteAttributeStat {
 type Record = HashMap<String, String>;
 type Stats = HashMap<String, HashMap<String, VoteAttributeStat>>;
 
+const ATTEMPTS: usize = 10;
+
 fn main() -> io::Result<()> {
     let mut reader = csv::Reader::from_path("./data/house-votes-84.csv")?;
 
-    let headers = reader.headers().expect("CSV headers missing!");
+    let headers = reader.headers().expect("CSV headers missing!").clone();
 
-    let mut raw_stats: Stats = headers
-        .iter()
-        .map(|header| (header.into(), HashMap::new()))
+    let mut records: Vec<Record> = reader
+        .deserialize::<Record>()
+        .filter_map(|r| r.ok())
         .collect();
 
-    let records = reader.deserialize::<Record>();
+    let mut rng = thread_rng();
+    records.shuffle(&mut rng);
 
-    // Populate stats
-    for record in records.filter_map(|r| r.ok()) {
-        for (attr, val) in record.iter() {
-            let attr_raw_stats = raw_stats.get_mut(attr).unwrap();
-            match attr_raw_stats.entry(val.into()) {
-                Vacant(entry) => {
-                    entry.insert(VoteAttributeStat::empty());
+    let chunk_size = records.len() / ATTEMPTS;
+
+    let mut hits = 0;
+    let mut misses = 0;
+    for attempt in 0..ATTEMPTS {
+        let mut raw_stats: Stats = headers
+            .iter()
+            .map(|header| (header.into(), HashMap::new()))
+            .collect();
+
+        let mut test_chunk: Vec<Record> = vec![];
+
+        // Populate stats
+        for (i, chunk) in records.chunks(chunk_size).enumerate() {
+            if i == attempt {
+                test_chunk = chunk.to_owned();
+                continue;
+            }
+            for record in chunk {
+                for (attr, val) in record.iter() {
+                    let attr_raw_stats = raw_stats.get_mut(attr).unwrap();
+                    match attr_raw_stats.entry(val.into()) {
+                        Vacant(entry) => {
+                            entry.insert(VoteAttributeStat::empty());
+                        }
+                        Occupied(entry) => {
+                            if record.get("Class Name").unwrap() == "democrat" {
+                                entry.into_mut().increment_left();
+                            } else {
+                                entry.into_mut().increment_right();
+                            }
+                        }
+                    };
                 }
-                Occupied(entry) => {
-                    if record.get("Class Name").unwrap() == "democrat" {
-                        entry.into_mut().increment_left();
-                    } else {
-                        entry.into_mut().increment_right();
-                    }
-                }
-            };
+            }
         }
+
+        for mut record in test_chunk {
+            let actual_vote = record.remove("Class Name").unwrap();
+            let classes = raw_stats.get("Class Name").unwrap();
+
+            let mut democrat_chance = record.iter().fold(1.0, |acc, (key, value)| {
+                let vote_stat = raw_stats.get(key).unwrap().get(value).unwrap();
+                acc * vote_stat.democrat as f32 / vote_stat.total as f32
+            });
+            democrat_chance *= classes["democrat"].total as f32;
+
+            let mut republican_chance = record.iter().fold(1.0, |acc, (key, value)| {
+                let vote_stat = raw_stats.get(key).unwrap().get(value).unwrap();
+                acc * vote_stat.republican as f32 / vote_stat.total as f32
+            });
+            republican_chance *= classes["republican"].total as f32;
+
+            let vote_prediciton = match democrat_chance < republican_chance {
+                true => "republican",
+                false => "democrat",
+            };
+
+            if vote_prediciton == actual_vote {
+                hits += 1;
+            } else {
+                misses += 1;
+            }
+        }
+
+        // println!(
+        //     "{}",
+        //     raw_stats
+        //         .iter()
+        //         .fold(String::new(), |acc, stat| acc + &format!("{:?}\n", stat))
+        // );
     }
 
-    println!(
-        "{}",
-        raw_stats
-            .iter()
-            .fold(String::new(), |acc, stat| acc + &format!("{:?}\n", stat))
-    );
+    println!("{} {}", hits, misses);
 
     Ok(())
 }
