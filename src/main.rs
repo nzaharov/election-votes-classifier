f475f8f13bec6d66474a872eb97f90633116218d
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-12-20 19:50:52 +0200

    different stats

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,72 @@
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 #[warn(clippy::all)]
 use std::collections::HashMap;
 use std::io;
 
+#[derive(Debug)]
+struct VoteAttributeStat {
+    republican: u32,
+    democrat: u32,
+    total: u32,
+}
+
+impl VoteAttributeStat {
+    fn empty() -> Self {
+        Self {
+            republican: 0,
+            democrat: 0,
+            total: 0,
+        }
+    }
+
+    fn increment_left(&mut self) {
+        self.democrat += 1;
+        self.total += 1;
+    }
+
+    fn increment_right(&mut self) {
+        self.republican += 1;
+        self.total += 1;
+    }
+}
+
 type Record = HashMap<String, String>;
-type Stats = HashMap<String, HashMap<String, u32>>;
+type Stats = HashMap<String, HashMap<String, VoteAttributeStat>>;
 
 fn main() -> io::Result<()> {
     let mut reader = csv::Reader::from_path("./data/house-votes-84.csv")?;
 
     let headers = reader.headers().expect("CSV headers missing!");
 
-    let mut stats: Stats = headers
+    let mut raw_stats: Stats = headers
         .iter()
         .map(|header| (header.into(), HashMap::new()))
         .collect();
 
     let records = reader.deserialize::<Record>();
 
+    // Populate stats
     for record in records.filter_map(|r| r.ok()) {
-        for (attr, val) in record.into_iter() {
-            let attr_stats = stats.get_mut(&attr).unwrap();
-            *attr_stats.entry(val).or_insert(0) += 1;
+        for (attr, val) in record.iter() {
+            let attr_raw_stats = raw_stats.get_mut(attr).unwrap();
+            match attr_raw_stats.entry(val.into()) {
+                Vacant(entry) => {
+                    entry.insert(VoteAttributeStat::empty());
+                }
+                Occupied(entry) => {
+                    if record.get("Class Name").unwrap() == "democrat" {
+                        entry.into_mut().increment_left();
+                    } else {
+                        entry.into_mut().increment_right();
+                    }
+                }
+            };
         }
     }
 
     println!(
         "{}",
-        stats
+        raw_stats
             .iter()
             .fold(String::new(), |acc, stat| acc + &format!("{:?}\n", stat))
     );
